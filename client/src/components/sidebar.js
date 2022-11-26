c25fca2731b3b81b5aca62b4cb4990d00e89b102
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-27 03:33:04 +0800

    Started the work on the front-end

---
+++ client/src/components/sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function sidebar() {
+  return (
+    <div>sidebar</div>
+  )
+}
+
---
+++ client/src/components/tabbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function tabbar() {
+  return (
+    <div>
+        <p>Tabbar</p>
+    </div>
+  )
+}
