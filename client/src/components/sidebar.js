bfd4ff1ca1a156e13880352c6168af4844938f8e
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-29 00:52:22 +0800

    Added styles to sidebar and buttons

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -6,10 +6,16 @@ export default function sidebar() {
     <div>
       <div>
         <ul>
-          <li><p>Home</p></li>
-          <li><p>Pages</p></li>
-          <li><p>Format</p></li>
-          <li><p>About</p></li>
+          <li className='breakline'><p>Home</p></li>
+          <li className='breakline'><p>Pages</p></li>
+          <li className='breakline'><p>Format</p></li>
+          <li>
+            <button className='sidebuttons'>Bold</button>
+            <button className='sidebuttons'>Italicize</button>
+            <button className='sidebuttons'>Bigger</button>
+            <button className='sidebuttons'>Smaller</button>
+            <button className='sidebuttons'>Colorize</button>
+          </li>
         </ul>
       </div>
     </div>
