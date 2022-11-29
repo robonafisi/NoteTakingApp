cf17c4f13390b0b86deaa66e6d0bf0d27dfc2122
Mohammad Nafisi <nafisidev@gmail.com>
2022-11-29 23:13:20 +0800

    Added the second route

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 
+
+//Homepage
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})  
+
+
+
+//Get the list of pages
 app.get('/pages', async(req, res)=>{
   try {
     const alltodos = await pool.query("SELECT * FROM pages_central");
@@ -19,9 +28,21 @@ app.get('/pages', async(req, res)=>{
   }
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })  
+//Create a page with content
+app.post('/newpage', async(req, res)=>{
+  try {
+    const { page_title, content, font_size, is_bold, is_italicised, is_colored } = req.body;
+
+    const newTodo = await pool.query(
+      "INSERT INTO pages_central VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [page_title, content, font_size, is_bold, is_italicised, is_colored]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(err.message);
+  }
+})
+
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
