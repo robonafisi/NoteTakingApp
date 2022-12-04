91602fee271f78a7c7a644166e19683414431c3c
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-04 23:32:47 +0800

    Working on exports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css';
+import { Sidebar } from './components/sidebar.js'; 
 
 export default function App(props) {
   return (
     <div className='container text-center'>
       <div>
+        <div>
+          <h1>{Sidebar}</h1>
+        </div>
         <div>
       <h1>
         {props.title}
