7dd3af7ff4f5d6f380cec576f6917e28a99e4c21
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-09 00:08:27 +0800

    Added the HTTP Request

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 
 export default function App() {
 
+  const [info, setInfo] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch(''),
-      const jsonresult = result.json();
+      const result = await fetch('http://localhost:3000/pages/10'),
+      const jsonresult = await  result.json();
+      setInfo(jsonresult);
     }
   },[])
+
+  console.log(info);
 
   return (
     <div className='container text-center'>
