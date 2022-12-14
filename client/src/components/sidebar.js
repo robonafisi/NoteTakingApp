4499930b8d6efbf637e25abc61fb7bad35b0f89a
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-14 23:59:17 +0800

    Adding more HTTP Stuff

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -17,24 +17,29 @@ export default function Sidebar() {
   };
 
   async function fetchData(id) {
-    try {
-      const http = require('http')
-      const data = JSON.stringify({
-        "id" : 11
-      });
-      const options = {
-        hostname : ,
-        path : ,
-        method : "POST",
-        header : {
-          'Content-Type':'application/json'
-        }
-      }
 
+      const http = require('https')
+      https.get('https://jsonplaceholder.typicode.com/users', res => {
+  let data = [];
+  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+  console.log('Status Code:', res.statusCode);
+  console.log('Date in Response header:', headerDate);
 
-    } catch (error) {
-      
+  res.on('data', chunk => {
+    data.push(chunk);
+  });
+
+  res.on('end', () => {
+    console.log('Response ended: ');
+    const users = JSON.parse(Buffer.concat(data).toString());
+
+    for(user of users) {
+      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
     }
+  });
+}).on('error', err => {
+  console.log('Error: ', err.message);
+});
   };
   
   useEffect(()=>{
