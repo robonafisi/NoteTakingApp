9a4143e14bd95dc43feeba73064843b3a24f051f
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-15 00:07:36 +0800

    Testing out fetch again

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -17,29 +17,22 @@ export default function Sidebar() {
   };
 
   async function fetchData(id) {
-
-      const http = require('https')
-      https.get('https://jsonplaceholder.typicode.com/users', res => {
-  let data = [];
-  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-  console.log('Status Code:', res.statusCode);
-  console.log('Date in Response header:', headerDate);
-
-  res.on('data', chunk => {
-    data.push(chunk);
-  });
-
-  res.on('end', () => {
-    console.log('Response ended: ');
-    const users = JSON.parse(Buffer.concat(data).toString());
-
-    for(user of users) {
-      console.log(`Got user with id: ${user.id}, name: ${user.name}`);
+    try {
+      fetch('http://localhost:5000/pagechange',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "id": id
+        }),
+      }).then(response => response.json())
+      .then(data => console.log(data));
+      
+  
+    } catch (error) {
+      
     }
-  });
-}).on('error', err => {
-  console.log('Error: ', err.message);
-});
   };
   
   useEffect(()=>{
