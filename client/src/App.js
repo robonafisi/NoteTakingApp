6be0f9f429e7b3196654830f81873dbb82fe3272
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-18 21:17:19 +0800

    Scrapping the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import logo from './logo.svg';
 import './App.css';
 
-
-export default function App() {
-
-  const [info, setInfo] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch('http://localhost:5000/pages/11');
-      const jsonresult = await  result.json();
-      setInfo(jsonresult);
-    }
-    fetchData();
-  },[])
-
+function App() {
   return (
-    <div className='container text-center'>
-      <div>
-        <div>
-        {info.map(information => (
-          <div>
-          <h1>{information.page_title}</h1>
-            <p>{information.content}</p>
-          </div>
-        ))}
-      <div>
-      </div>
-      </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
+
+export default App;
